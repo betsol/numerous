4ac4282cbbabf3a117d40640a1e9348c4b2ef84f
Slava Fomin II <s.fomin@betsol.ru>
2016-09-17 22:43:05 +0300

    Updated demo page to demonstrate use of multiple locales.

--- demos/script.js
+++ demos/script.js
@@ -2,11 +2,57 @@
 
   'use strict';
 
+  // List of pluralization variants for each supported locale.
+  var VARIANTS = {
+    APPLES: {
+      en: {
+        one: 'apple',
+        other: 'apples'
+      },
+      ru: {
+        one: 'яблоко',
+        few: 'яблока',
+        many: 'яблок'
+      }
+    },
+    ORANGES: {
+      en: {
+        one: 'orange',
+        other: 'oranges'
+      },
+      ru: {
+        one: 'апельсин',
+        few: 'апельсина',
+        many: 'апельсинов'
+      }
+    }
+  };
+
+  // List of additional texts for each supported locale.
+  var TEXTS = {
+    en: {
+      'i-have': 'I have',
+      'and': 'and'
+    },
+    ru: {
+      'i-have': 'У меня есть',
+      'and': 'и'
+    }
+  };
+
+  // Listening for for form submission.
   document.getElementById('form').addEventListener('submit', function (event) {
     event.preventDefault();
     format();
   });
 
+  // Listening for for locale change.
+  document.getElementById('locale').addEventListener('input', function (event) {
+    event.preventDefault();
+    format();
+  });
+
+  // Initially formatting the result.
   format();
 
 
@@ -15,19 +61,33 @@
     return parseInt(string);
   }
 
+  function getStringFromInput (id) {
+    return document.getElementById(id).value;
+  }
+
   function format () {
+
+    // Reading locale from form.
+    var locale = getStringFromInput('locale');
+
+    // Pluralizing apples.
     var applesCount = getNumberFromInput('apples-count');
-    var orangesCount = getNumberFromInput('oranges-count');
-    var applesResult = numerous.pluralize('en', applesCount, {
-      one: 'apple',
-      other: 'apples'
-    });
-    var orangesResult = numerous.pluralize('en', orangesCount, {
-      one: 'orange',
-      other: 'oranges'
-    });
+    var applesResult = numerous.pluralize(locale, applesCount, VARIANTS.APPLES[locale]);
     document.getElementById('apples-result').innerHTML = applesCount + ' ' + applesResult;
+
+    // Pluralizing oranges.
+    var orangesCount = getNumberFromInput('oranges-count');
+    var orangesResult = numerous.pluralize(locale, orangesCount, VARIANTS.ORANGES[locale]);
     document.getElementById('oranges-result').innerHTML = orangesCount + ' ' + orangesResult;
+
+    // Updating other text.
+    var textStore = TEXTS[locale];
+    for (var key in textStore) {
+      if (textStore.hasOwnProperty(key)) {
+        document.getElementById('text-' + key).innerHTML = textStore[key];
+      }
+    }
+
   }
 
 })();
